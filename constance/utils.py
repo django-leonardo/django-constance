fc6d0b5c91c86a92fe49aef6b598ad977b387810
Michael Kutý <6du1ro.n@gmail.com>
2015-09-26 12:21:50 +0200

    fix missed function

--- constance/utils.py
+++ constance/utils.py
@@ -3,3 +3,8 @@ try:
     from importlib import import_module
 except ImportError:
     from django.utils.importlib import import_module
+
+
+def import_module_attr(path):
+    package, module = path.rsplit('.', 1)
+    return getattr(import_module(package), module)
