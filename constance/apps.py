03609f2ca5dd88bd5800a3f27f9d4c66493df8ea
Michael Kutý <6du1ro.n@gmail.com>
2015-10-18 14:49:39 +0200

    load dictionaries from strings

--- constance/apps.py
+++ constance/apps.py
@@ -2,6 +2,17 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings as django_settings
 
+import json
+
+
+def _load_dict(value):
+    try:
+        value = json.loads(value)
+    except:
+        # TODO log me here
+        pass
+    return value
+
 
 class ConstanceConfig(AppConfig):
     name = 'constance'
@@ -14,9 +25,9 @@ class ConstanceConfig(AppConfig):
 
             for k in dir(config):
                 # just only if is not present in settings
+
                 if k not in dir(django_settings):
-                    setattr(django_settings, k, getattr(config, k))
-        except Exception:
-            # in some environment may failed
-            # swallowed
+                    setattr(django_settings, k, _load_dict(getattr(config, k)))
+        except:
+            # TODO: log me here
             pass
